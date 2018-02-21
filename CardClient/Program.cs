af2694420eadcf3e08b54357ccc8cfdf05be5a94
wkspeaker <speaker_k@126.com>
2018-02-21 15:51:11 +0800

    V1
    创建CardLib的类库以及相应的控制台客户端。
    Ref:10.6 (P224)

---
+++ CardClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardLib;
+
+namespace CardClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck myDeck = new Deck();
+            myDeck.Shuffle();
+
+            for (int i = 0; i < 52; i++)
+            {
+                Card tempCard = myDeck.GetCard(i);
+                Console.Write(tempCard.ToString());
+                if (i != 51)
+                    Console.Write(",  ");
+                else
+                    Console.WriteLine();
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
